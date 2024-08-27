1f60a52617454a02232fd482b2a8509593ddccaf
unknown <emtechdev79@gmail.com>
2024-08-27 12:48:53 +0300

    improve performance

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import Category, SubCategory, Product, Pricing, ProductSpesfication, File
-# Register your models here.
+from .models import Category, SubCategory, Product, Pricing, ProductSpesfication, File, UserProfile
+
+
+
 
 admin.site.register(Category)
 admin.site.register(SubCategory)
 admin.site.register(Product)
 admin.site.register(File)
+admin.site.register(UserProfile)
